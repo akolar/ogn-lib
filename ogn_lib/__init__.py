e7b1fa9410cae4fd94c0d91b5f2c0a9ed0e5cb09
Anze Kolar <me@akolar.com>
2017-09-27 15:48:12 +0200

    Add constants to __init__.py

--- ogn_lib/__init__.py
+++ ogn_lib/__init__.py
@@ -1,5 +1,6 @@
 from ogn_lib.client import OgnClient  # noqa: F401
 from ogn_lib.parser import Parser  # noqa: F401
+from ogn_lib.constants import AirplaneType, AddressType, BeaconType  # noqa: F401
 
 __title__ = 'ogn-lib'
 __description__ = 'Beacon processor for the OGN data stream.'
--- tests/test_ogn_lib.py
+++ tests/test_ogn_lib.py
@@ -4,3 +4,6 @@ import ogn_lib
 def test_entry_points():
     ogn_lib.OgnClient
     ogn_lib.Parser
+    ogn_lib.AirplaneType
+    ogn_lib.AddressType
+    ogn_lib.BeaconType
